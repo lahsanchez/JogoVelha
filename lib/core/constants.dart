d4dbf36002f778b4ee474f649a37bd095516b825
lahsanchez <49082097+lahsanchez@users.noreply.github.com>
2020-11-25 19:04:38 -0300

    Add files via upload

---
+++ lib/core/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const int BOARD_SIZE = 9;
+
+const Color PLAYER1_COLOR = Colors.red;
+const Color PLAYER2_COLOR = Colors.blue;
+
+const String PLAYER1_SYMBOL = 'X';
+const String PLAYER2_SYMBOL = 'O';
+
+const String GAME_TITLE = 'Jogo da Velha ';
+const String TIED_TITLE = 'Jogo Empatado!';
+const String WIN_TITLE = 'Jogador [SYMBOL] venceu!';
+const String DIALOG_MESSAGE =
+    'Pressione o botão de reset para começar novamente!';
+const String SINGLE_PLAYER_MODE_LABEL = '1 Player';
+const String MULTIPLAYER_MODE_LABEL = '2 Players';
+const String RESET_BUTTON_LABEL = 'RESET';
